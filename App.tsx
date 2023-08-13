76f19e6e977a1abde71317cc5a2fca64c2570924
MrJooka <mr.jooka@gmail.com>
2023-08-14 03:21:09 +0900

    3.1.3 StatusBar 영역

--- App.tsx
+++ App.tsx
@@ -6,17 +6,20 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import DateHead from './components/DateHead';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 function App(): JSX.Element {
   const today = new Date();
 
   console.log(today);
   return (
-    <SafeAreaView>
-      <DateHead date={today} />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']}>
+        <DateHead date={today} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
--- components/DateHead.tsx
+++ components/DateHead.tsx
@@ -1,5 +1,6 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 type Props = {
   date: Date;
@@ -9,16 +10,30 @@ const DateHead = ({date}: Props) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
+
+  const {top} = useSafeAreaInsets();
   return (
-    <View style={styles.block}>
-      <Text style={styles.dateText}>{`${year}년 ${month}월 ${day}일`}</Text>
-    </View>
+    <>
+      <View
+        style={[
+          styles.statusBarPlaceholder,
+          {
+            height: top,
+          },
+        ]}
+      />
+      <StatusBar backgroundColor="#26a69a" barStyle="light-content" />
+      <View style={styles.block}>
+        <Text style={styles.dateText}>{`${year}년 ${month}월 ${day}일`}</Text>
+      </View>
+    </>
   );
 };
 
 export default DateHead;
 
 const styles = StyleSheet.create({
+  statusBarPlaceholder: {backgroundColor: '#26a69a'},
   block: {padding: 16, backgroundColor: '#26a69a'},
   dateText: {fontSize: 24, color: '#fff'},
 });
